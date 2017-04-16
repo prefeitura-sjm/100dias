5a852d6efe7408ed85589d188c26dc977db7fa2e
Igor Felipe <igor_play_22@hotmail.com>
2017-04-16 03:16:33 -0300

    Efeito de contagem apenas quando passar pela section

--- dev/scripts/counterItens.js
+++ dev/scripts/counterItens.js
@@ -1,10 +1,24 @@
+
+$(function() {
+    var oTop = $('.counter-wrapper').offset().top - window.innerHeight;
+    $(window).scroll(function(){
+
+        var pTop = $('body').scrollTop();
+        console.log( pTop + ' - ' + oTop );   //just for your debugging
+        if( pTop > oTop ){
+            counterItensFun();
+        }
+    });
+});
 function counterEffect(elem){
   var counterItemValue = elem.getAttribute('data-value');
   setTimeout(function(){
     elem.innerHTML = counterItemValue;
   }, 1000);
 }
-var counterItens = document.querySelectorAll('.odometer');
-for(i = 0; i<counterItens.length; i++){
-  counterEffect(counterItens[i]);
+function counterItensFun(){
+  var counterItens = document.querySelectorAll('.odometer');
+  for(i = 0; i<counterItens.length; i++){
+    counterEffect(counterItens[i]);
+  }
 }
